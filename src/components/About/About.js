6d372d4c53273ffb25e228c087c70065739705c5
CodeLewisYates <lewis_yates@ymail.com>
2021-03-23 17:27:14 +0000

    Initial

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = (props) => {
+  return (
+    <div className="AboutContainer">
+      {/* All html for about goes here */}
+      <h1>ABOUT</h1>
+    </div>
+  );
+};
+export default About;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <div className="HeaderContainer">
+      {/* All html for header goes here */}
+      <h1>HEADER</h1>
+    </div>
+  );
+};
+
+export default Header;
